bc62f7662bce35fbbf25e7db000d88531a896664
JrnJ <jeroenmilj>
2025-02-07 15:36:44 +0100

    basics clicker

---
+++ CookieClicker/StoreView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreView.swift
+//  CookieClicker
+//
+//  Created by Jeroen Jochems on 07/02/2025.
+//
+
+import SwiftUI
+
+struct StoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreView()
+}
